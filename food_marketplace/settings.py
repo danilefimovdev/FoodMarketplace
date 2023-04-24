598aaa7c4b10cad6c590506f7ce71d4f6965d20c
danil.dev <danilefimov.dev@gmail.com>
2023-04-24 22:57:09 +0500

    set user model

--- food_marketplace/settings.py
+++ food_marketplace/settings.py
@@ -29,6 +29,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -76,6 +78,7 @@ DATABASES = {
     }
 }
 
+AUTH_USER_MODEL = 'accounts.User'
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
